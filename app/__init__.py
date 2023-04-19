7f80a82124640a78acb4bb04d5ddefb0204c6f7a
Jamila McGowan <jamcarr280@gmail.com>
2023-04-18 18:33:38 -0700

    defined the models, created forms, started register, login and logout route.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config import Config
+from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+csrf = CSRFProtect(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 from app import views
