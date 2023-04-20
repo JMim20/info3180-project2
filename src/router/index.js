582f74fb1423c8685ac4256341fe51ca731f0128
caysi morgan <caysi.s.morgan@gmail.com>
2023-04-19 23:15:01 -0500

    router adjusted

--- src/router/index.js
+++ src/router/index.js
@@ -10,13 +10,33 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/register',
+      name: 'register',
+      //Accepts user information and saves it to the database
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/RegisterView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      //Accepts login credentials as username and password
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/explore',
+      name: 'explore',
+      //View/Explore all posts by all users
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ExploreView.vue')
     }
+
   ]
 })
 
