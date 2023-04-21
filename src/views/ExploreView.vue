38ae60a58e75fb533ed46f61f82822198922ed12
caysi morgan <caysi.s.morgan@gmail.com>
2023-04-21 02:27:44 -0500

    views adjusted

---
+++ src/views/ExploreView.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <div class ="container-explore">
+        <h2>Explore </h2>
+        <ExploreForm/>
+
+    </div>
+</template>
+<script>
+import ExploreForm from "../components/ExploreForm.vue"
+export default{
+    name: "ExploreView",
+    components:{
+        ExploreForm
+    }
+}
+</script>
+<style>
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class = "container-login">
+        <h2> Login <h2>
+            <LoginForm/>
+    </div>
+</template>
+<script>
+import LoginForm from "../components/LoginForm.vue";
+export default{
+    name:"LoginView",
+    components:{ LoginForm}
+}
+</script>
+<style>
+</style>
